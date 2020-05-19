618f669b9feb53c9fdfb46f7991b742fa34062b3
Jesús Gómez <jgomo3@gmail.com>
2020-05-19 10:15:37 -0230

    Extract display dispatcher logic from entrypoint

--- wikipit.py
+++ wikipit.py
@@ -10,7 +10,8 @@ import webbrowser
 import wikipedia
 import click
 
-OFFSET = 2
+
+# Displayer
 
 def delegate_to_browser(address):
     webbrowser.open(address, autoraise=True)
@@ -18,14 +19,25 @@ def delegate_to_browser(address):
 def display_raw(content):
     print(content)
 
-def display_with_fixed_width(lines, content):
+def display_with_fixed_width(content, lines, offset=2):
     sizes = shutil.get_terminal_size((80, 20))
-    columns = sizes.columns - OFFSET
+    columns = sizes.columns - offset
 
     rows = textwrap.wrap(content, columns)
 
     for row in rows[0:lines]:
         print(row)
+
+def display_by_options(content, lines, all_, browser):
+    method = display_raw
+    if browser:
+        method = delegate_to_browser
+    elif not all_ and lines:
+        method = partial(display_with_fixed_width, lines=lines)
+    method(content)
+    
+
+# Searcher
 
 def address_from_keywords(keywords):
     return wikipedia.page(keywords).url
@@ -36,16 +48,17 @@ def summary_from_keywords(keywords):
 def content_from_keywords(keywords):
     return wikipedia.page(keywords).content
 
-def searcher(keywords, lines, all_, browser):
+def search_by_options(keywords, all_, browser): # Kind of the Controller
+    method = summary_from_keywords
     if browser:
-        return address_from_keywords(keywords)
-    if all_:
-        return content_from_keywords(keywords)
+        method = address_from_keywords
+    elif all_:
+        method = content_from_keywords
 
-    result = summary_from_keywords(keywords)
+    return method(keywords)
 
-    return result
 
+# System
 
 @click.command()
 @click.argument('keywords')
@@ -53,17 +66,15 @@ def searcher(keywords, lines, all_, browser):
 @click.option('--all', '-a', 'all_', is_flag=True, help="Print the entire page content.")
 @click.option('--browser', '-b', is_flag=True, help="Open in browser.")
 @click.option('--lang', '-e', default="en", help="Open in browser.")
-def wiki(keywords, lines, all_=None, browser=None, lang="en"):
+def wiki(keywords, lines, all_=False, browser=False, lang="en"):
 
+    # Config
     wikipedia.set_lang(lang)
+    display = partial(display_by_options, lines=lines, all_=all_, browser=browser)
+    search = partial(search_by_options, all_=all_, browser=browser)
 
-    display = display_raw
-    if browser:
-        display = delegate_to_browser
-    elif not all_ and lines:
-        display = partial(display_with_fixed_width, lines)
-    
-    display(searcher(keywords, lines, all_, browser))
+    # Excecution
+    display(search(keywords))
 
 
 if __name__ == "__main__":
